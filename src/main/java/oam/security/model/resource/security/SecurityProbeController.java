0c2a6bc32f3b4465cf11207df29592570cda056d
WeiChiang <wejohnwang@gmail.com>
2023-05-19 11:42:20 +0800

    [1.5.19.1] 單機可傳送圖片版本

--- src/main/java/oam/security/model/resource/security/SecurityProbeController.java
+++ src/main/java/oam/security/model/resource/security/SecurityProbeController.java
@@ -1,7 +1,10 @@
 package oam.security.model.resource.security;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -9,11 +12,15 @@ import java.net.Socket;
 import java.net.URISyntaxException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,6 +31,13 @@ public class SecurityProbeController {
 	
 	@Autowired
 	private SecurityProbeService securityProbeService;
+	
+	@Value("${solaris.session.dn-service}")
+	private String dn_service;
+	@Value("${solaris.session.dn-socketPort}")
+	private int dn_socketPort;
+	@Value("${solaris.session.manage-service}")
+	private String manage_service;
 	
 	@GetMapping("/testSocketBindInterface")
 	@ResponseStatus(HttpStatus.OK)
@@ -56,6 +70,54 @@ public class SecurityProbeController {
 
         socket.close();
         System.out.println("Connection closed");
+	}
+	
+	@PostMapping("/transferImage")
+	@ResponseStatus(HttpStatus.OK)
+	private void transferImage(/*body參數(附帶標註)*/) throws Exception {
+		//與DN端建立socket
+		//先找出自己的網卡IP
+//		String ueIp = securityProbeService.getUeransimProbeIp();
+		String ueIp = "192.168.50.209";
+        InetAddress inetAddress = InetAddress.getByName(ueIp);
+		
+        //找出server
+//        String iperfTimeStamp = content.get("iperf3").get("timestamp").asText();
+//        String serverAddress = dn_service + "-" + iperfTimeStamp; // DN的server
+        String serverAddress = "localhost"; // DN的server
+        //進行socket連線
+//        Socket socket = new Socket(serverAddress, dn_socketPort, inetAddress, 0);
+        Socket socket = new Socket(serverAddress, dn_socketPort);
+        System.out.println("Connected to server on " + socket.getRemoteSocketAddress());
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        
+        out.println("Hello!");
+        
+        // 建立輸入串流，用於讀取圖片檔案
+        InputStream inputStream = new FileInputStream("image.jpg");
+        
+        // 建立緩衝區
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        // 建立輸出串流，用於發送圖片
+        OutputStream imageOutputStream = socket.getOutputStream();
+        
+        // 發送圖片
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            imageOutputStream.write(buffer, 0, bytesRead);
+        }
+
+        System.out.println("圖片發送完成");
+        
+        // 關閉串流和Socket連接
+        imageOutputStream.close();
+        inputStream.close();
+        out.close();
+        socket.close();
+        
+        
 	}
 
 }
